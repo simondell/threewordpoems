bd810a9a9a68faaa9cd2dd13146aaca889fd2c98
Simon Dell <simon@expressionworks.co.uk>
2014-12-03 15:46:49 +0000

    Added two seed Poets

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Poet.destroy_all
+
+Poet.create!([{
+	display_name: "Anon E Mousse",
+	email: "anon_e_mousse@threewordpoems.com",
+	first_name: "Anon",
+	last_name: "Mousse"
+
+	},{
+	display_name: "The Other Dell",
+	email: "simon@theotherdell.com",
+	first_name: "Simon",
+	last_name: "Dell"
+	}])
