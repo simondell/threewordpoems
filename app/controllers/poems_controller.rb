ffd72bfc7d74f2f280a327c845df2ca3fad968fa
Simon Dell <simon@expressionworks.co.uk>
2014-12-16 10:33:12 +0000

    Include categories in poem queries.

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -4,7 +4,7 @@ class PoemsController < ApplicationController
   # GET /poems
   # GET /poems.json
   def index
-    @poems = Poem.categorise_by( params[:categorise_by] )
+    @poems = Poem.includes( :categories ).categorise_by( params[:categorise_by] )
   end
 
   # GET /poems/1
