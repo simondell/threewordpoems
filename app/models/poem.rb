68c51474831d61342b7dd814c962dfe508ad3746
Simon Dell <simon@expressionworks.co.uk>
2014-12-16 09:28:43 +0000

    Added 'categorised_by' scope to Poem.

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -3,5 +3,6 @@ class Poem < ActiveRecord::Base
 	has_many :poem_categories
 	has_many :categories, through: :poem_categories
 
-	scope :top_ten, -> { where top_ten: true }
+	# scope :top_ten, -> { where top_ten: true }
+	scope :categorise_by, -> ( category_name ) { joins( :categories ).where( 'categories.name = ?', category_name ) if category_name.present? }
 end
