1817555923b89bfffc08adbc9907d62ddbf69af2
Simon Dell <simon@expressionworks.co.uk>
2015-05-19 14:01:02 +0100

    Search results list is readable. Links from results to individual poems#show works. poems#show works and doesn't currently do anything but show a poem. Application layout updated slightly to accomodate the previous points.

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,12 +1,12 @@
 class PoemsController < ApplicationController
 	before_action :set_poem, only: [:show, :edit, :update, :destroy]
 	before_action :set_poets, only: [:edit, :update]
+	before_action :set_search_for, only: [:index, :show]
 
 	# GET /poems
 	# GET /poems.json
 	def index
 		# @poems = Poem.includes( :categories ).categorise_by( params[:categorise_by] )
-		@search_for = params[:search_for]
 		@poems = Poem.search( params[:search_for] ) unless @search_for.nil?
 
 		# cache for later
@@ -16,20 +16,21 @@ class PoemsController < ApplicationController
 	# GET /poems/1
 	# GET /poems/1.json
 	def show
-		@current_id = params[ :id ].to_i
-		@coll = session[:collection] || nil
 
-		unless @coll == nil
-			@current = @coll.find_index( @current_id ) || nil
+		# @current_id = params[ :id ].to_i
+		# @coll = session[ :collection ] || nil
 
-			if @current + 1 < @coll.size
-				@next_poem = Poem.find( @coll[ @current + 1 ] )
-			end
+		# unless @coll == nil
+		# 	@current = @coll.find_index( @current_id ) || nil
 
-			if @current - 1 >= 0
-				@prev_poem = Poem.find( @coll[ @current - 1 ] )
-			end
-		end # unless
+		# 	if @current + 1 < @coll.size
+		# 		@next_poem = Poem.find( @coll[ @current + 1 ] )
+		# 	end
+
+		# 	if @current - 1 >= 0
+		# 		@prev_poem = Poem.find( @coll[ @current - 1 ] )
+		# 	end
+		# end # unless
 	end
 
 	# GET /poems/new
@@ -92,6 +93,10 @@ class PoemsController < ApplicationController
 
 		def set_poets
 			@poets = Poet.all
+		end
+
+		def set_search_for
+			@search_for = params[:search_for]
 		end
 
 		# Never trust parameters from the scary internet, only allow the white list through.
