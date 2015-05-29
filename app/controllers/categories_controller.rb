c44e719f1b271c5012ab665f513c47db144de39e
Simon Dell <simon@expressionworks.co.uk>
2015-05-29 13:56:29 +0100

    Tried to add prev/next to category#show. Causes error when you follow the link to 'next'

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,21 @@
 class CategoriesController < ApplicationController
 	def show
-		@poem = Category.first.poems.first
+
+		@poems = Category.first.poems
+		@poem = @poems.first
+
+		current_poem_index = @poems.find_index @poem
+
+		unless @poems == nil
+			if current_poem_index + 1 < @poems.size
+				@next_poem = @poems[ current_poem_index + 1 ]
+			end
+
+			if current_poem_index - 1 >= 0
+				@prev_poem = @poems[ current_poem_index - 1 ]
+			end
+		end # unless
+
 		render "poems/show"
 	end
 end
