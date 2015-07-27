440a1e8e57a1cb952caa41bd4a0c9004cecd324d
Simon Dell <simon@expressionworks.co.uk>
2015-07-27 13:03:03 +0100

    Added email validity tests & an implementation, following Rails Tutorial 6.2.4

--- app/models/poet.rb
+++ app/models/poet.rb
@@ -1,10 +1,13 @@
 class Poet < ActiveRecord::Base
+
 	has_many :poems
 
 	validates :name,
 		presence: true
 
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@([a-z\d\-]+\.)+[a-z]+\z/i
 	validates :email,
 		presence: true,
-		length: { maximum: 255 }
+		length: { maximum: 255 },
+		format: { with: VALID_EMAIL_REGEX }
 end
--- test/models/poet_test.rb
+++ test/models/poet_test.rb
@@ -31,4 +31,41 @@ class PoetTest < ActiveSupport::TestCase
 
     assert_not @poet.valid?
   end
+
+
+  test 'email should accept valid email addresses' do
+    valid_addresses = %w[
+      user@example.com
+      USER@foo.COM
+      A_US-ER@foo.bar.org
+      first.last@foo.jp
+      alice+bob@baz.cn
+    ]
+
+    valid_addresses.each do |valid_address|
+      @poet.email = valid_address
+
+      assert @poet.valid?, '${valid_address.inspect} should be valid'
+    end
+  end
+
+
+  test 'email should refuse invalid email addresses' do
+    invalid_addresses = %w[
+      user@example,com
+      user@example..com
+      user_at_foo.org
+      user.name@example.
+      foo@bar_baz.com
+      foo@bar+baz.com
+      foo@bar+baz.com
+    ]
+
+    invalid_addresses.each do |invalid_address|
+      @poet.email = invalid_address
+
+      assert_not @poet.valid?, '${invalid_address.inspec} should be invalid'
+    end
+  end
+
 end
