743d0e5c13668e9c919453a5a70b0f7f7f4d0f89
Simon Dell <simon@expressionworks.co.uk>
2015-08-13 13:47:23 +0100

    Added a gravatar to the poets and the poem credits

--- app/helpers/poets_helper.rb
+++ app/helpers/poets_helper.rb
@@ -1,2 +1,10 @@
 module PoetsHelper
+
+  # Returns the Gravatar for the given user.
+  def gravatar_for(poet)
+    gravatar_id = Digest::MD5::hexdigest(poet.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: poet.name, class: "gravatar")
+  end
+
 end
