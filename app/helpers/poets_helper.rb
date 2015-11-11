d45a7f6fe7b69aed41b09770016ed6e3eba763a9
Simon Dell <simon@expressionworks.co.uk>
2015-11-11 11:30:43 +0000

    7.5.1 gravatars shrunk.

--- app/helpers/poets_helper.rb
+++ app/helpers/poets_helper.rb
@@ -1,9 +1,10 @@
 module PoetsHelper
 
   # Returns the Gravatar for the given user.
-  def gravatar_for(poet)
+  def gravatar_for( poet, options = { size: 80 } )
     gravatar_id = Digest::MD5::hexdigest(poet.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: poet.name, class: "gravatar")
   end
 
