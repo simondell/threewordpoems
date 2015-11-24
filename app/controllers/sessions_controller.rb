cf289f1e2c624c60c764219f93f9021dac56de33
Simon Dell <simon@expressionworks.co.uk>
2015-11-24 15:00:49 +0000

    8.1 failed log-in path works, and has an integration test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	submitted_email = params[:session][:email].downcase
+  	submitted_password = params[:session][:password]
+
+  	poet = Poet.find_by email: submitted_email
+
+  	if poet && poet.authenticate( submitted_password )
+  		# do stuff
+  	else
+  		flash.now[:danger] = 'Invalid email/password combination'
+  		render 'new'
+  	end
+  end
 end
---
+++ test/integration/poets_login_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class PoetsLoginTest < ActionDispatch::IntegrationTest
+
+	test 'log in with invalid information' do
+		get login_path
+		assert_template 'sessions/new'
+
+		post login_path, session: { email: '', password: '' }
+		assert_template 'sessions/new'
+		assert_not flash.empty?
+
+		get root_path
+		assert flash.empty?
+	end
+
+end
