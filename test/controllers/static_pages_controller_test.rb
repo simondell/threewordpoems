9560f41496f3c69279bde00e2510e12381182dfe
Simon Dell <simon@expressionworks.co.uk>
2016-07-08 00:40:18 +0100

    3.4.1 red again
    Temporarily moved application layout to prove a point.
    Added tests for page titles.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select 'title', 'Three Word Poems - Home'
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select 'title', 'Three Word Poems - About'
   end
 
   test "should get styleguide" do
     get static_pages_styleguide_url
     assert_response :success
+    assert_select 'title', 'Three Word Poems - Styleguide'
   end
 
 end
