a910b25d8124fcd5d1b6d06527653db7dd82e640
Simon Dell <simon@expressionworks.co.uk>
2016-07-08 00:47:32 +0100

    EX 3.4.2.1 dryer tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select 'title', 'Three Word Poems - Home'
+  def setup
+    @base_title = 'Three Word Poems -'
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select 'title', 'Three Word Poems - About'
+    assert_select 'title', "#{@base_title} About"
+  end
+
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select 'title', "#{@base_title} Home"
   end
 
   test "should get styleguide" do
     get static_pages_styleguide_url
     assert_response :success
-    assert_select 'title', 'Three Word Poems - Styleguide'
+    assert_select 'title', "#{@base_title} Styleguide"
   end
 
 end
