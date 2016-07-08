4c3e0283e4f91ae2ca44f9b421981fd574b5582f
Simon Dell <simon@expressionworks.co.uk>
2016-07-08 19:20:17 +0100

    4.1 helpers
    Added a helper for page titles. Updated tests to reflect a new pattern
    for page titles (the home page doesn't have a page title other than the
    site title).

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,37 +2,37 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @base_title = 'Three Word Poems -'
+    @base_title = 'Three Word Poems'
   end
 
   test "should get root" do
     get root_url
     assert_response :success
-    assert_select 'title', "#{@base_title} Home"
+    assert_select 'title', "#{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select 'title', "#{@base_title} About"
+    assert_select 'title', "#{@base_title} - About"
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select 'title', "#{@base_title} Contact"
+    assert_select 'title', "#{@base_title} - Contact"
   end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select 'title', "#{@base_title} Home"
+    assert_select 'title', "#{@base_title}"
   end
 
   test "should get styleguide" do
     get static_pages_styleguide_url
     assert_response :success
-    assert_select 'title', "#{@base_title} Styleguide"
+    assert_select 'title', "#{@base_title} - Styleguide"
   end
 
 end
