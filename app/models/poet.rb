4e69d5d3c142194a36ee0257a2cfa8cca5436365
Simon Dell <simon@expressionworks.co.uk>
2016-07-12 18:38:40 +0100

    6.1.1 Poet model
    You say users, I say POETS. POETS, POETS POETS!!

---
+++ app/models/poet.rb
@@ -0,0 +1,2 @@
+class Poet < ApplicationRecord
+end
---
+++ db/migrate/20160712173800_create_poets.rb
@@ -0,0 +1,10 @@
+class CreatePoets < ActiveRecord::Migration[5.0]
+  def change
+    create_table :poets do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
