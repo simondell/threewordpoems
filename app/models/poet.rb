947f772d3700f2b6db3603ef83369d62af9047da
Simon Dell <simon@expressionworks.co.uk>
2016-07-14 14:04:46 +0100

    6.2.3 length constraints

--- app/models/poet.rb
+++ app/models/poet.rb
@@ -1,2 +1,9 @@
 class Poet < ApplicationRecord
+  validates :name,
+    presence: true,
+    length: { maximum: 1057 }
+
+  validates :email,
+    presence: true,
+    length: { maximum: 400 }
 end
--- test/models/poet_test.rb
+++ test/models/poet_test.rb
@@ -10,8 +10,25 @@ class PoetTest < ActiveSupport::TestCase
     assert @poet.valid?
   end
 
-  test 'another_poet should be valid' do
-    another_poet = Poet.new
-    assert another_poet.valid?
+  # test 'another_poet should be valid' do
+  #   another_poet = Poet.new
+  #   assert another_poet.valid?
+  # end
+
+  # name constraints
+  test 'poet.name cant be empty' do
+    @poet.name = '    '
+    assert_not @poet.valid?
+  end
+
+  test 'name can\'t be toooooo long' do
+    @poet.name = 'Redd Wacky League Antlez Broke the Stereo Neon Tide Bring Back Honesty Coalition Feedback Hand of Aces Keep Going Captain Let’s Pretend Lost State of Dance Paper Taxis Lunar Road Up! Down! Strange! All and I Neon Sheep Eve Hornby Faye Bradley AJ Wilde Michael Rice Dion Watts Matthew Appleyard John Ashurst Lauren Swales Zoe Angus Jaspreet Singh Emma Matthews Nicola Brown Leanne Pickering Victoria Davies Rachel Burnside Gil Parker Freya Watson Alisha Watts James Pearson Jacob Sotheran-Darley Beth Lowery Jasmine Hewitt Chloe Gibson Molly Farquhar Lewis Murphy Abbie Coulson Nick Davies Harvey Parker Kyran Williamson Michael Anderson Bethany Murray Sophie Hamilton Amy Wilkins Emma Simpson Liam Wales Jacob Bartram Alex Hooks Rebecca Miller Caitlin Miller Sean McCloskey Dominic Parker Abbey Sharpe Elena Larkin Rebecca Simpson Nick Dixon Abbie Farrelly Liam Grieves Casey Smith Liam Downing Ben Wignall Elizabeth Hann Danielle Walker Lauren Glen James Johnson Ben Ervine Kate Burton James Hudson Daniel Mayes Matthew Kitching Josh Bennett Evolution Dreams'
+    assert_not @poet.valid?
+  end
+
+  # email constraints
+  test 'poet.email cant be empty' do
+    @poet.email = '    '
+    assert_not @poet.valid?
   end
 end
