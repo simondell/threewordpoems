abf1f21e0955796d3545fb7aa8db32559a143d85
Simon Dell <simon@expressionworks.co.uk>
2016-07-14 13:25:28 +0100

    6.2.1
    Beginning poet validity tests

--- test/models/poet_test.rb
+++ test/models/poet_test.rb
@@ -1,6 +1,14 @@
 require 'test_helper'
 
 class PoetTest < ActiveSupport::TestCase
+
+  def setup
+    @poet = Poet.new name: 'The testing poet', email: 'three_word_tester@threewordpoems.com'
+  end
+
+  test 'should be valid' do
+    assert @poet.valid?
+  end
   # test "the truth" do
   #   assert true
   # end
