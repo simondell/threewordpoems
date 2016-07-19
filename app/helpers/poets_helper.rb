6b3a8526c0cd1239a3d60935c1b94367b69c1f83
Simon Dell <simon@expressionworks.co.uk>
2016-07-19 22:37:42 +0100

    7.1.4 ex 1, 2 Gravatar
    Tutorial directed me to add a Gravatar. I want this, but I don't want
    the mark-up or CSS the tutorial suggested.

--- app/helpers/poets_helper.rb
+++ app/helpers/poets_helper.rb
@@ -1,2 +1,9 @@
 module PoetsHelper
+
+  def gravatar_for poet, options = { size: 50 }
+    id = Digest::MD5::hexdigest poet.email.downcase
+    size = options[:size]
+    url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag url, alt: ''
+  end
 end
