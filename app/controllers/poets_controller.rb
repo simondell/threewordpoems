8949e0c39a9901f5bec9759cf4acb5763ff86fc1
Simon Dell <simon@expressionworks.co.uk>
2016-07-19 13:49:30 +0100

    7.1.2 ex 1, 2 Showing a poet
    Add param debugging, poet/user metatdata to a "show" page. Handy. This
    will end up as a Poet's profile page.

--- app/controllers/poets_controller.rb
+++ app/controllers/poets_controller.rb
@@ -1,4 +1,8 @@
 class PoetsController < ApplicationController
   def new
   end
+
+  def show
+    @poet = Poet.find params[:id]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/styleguide', to: 'static_pages#styleguide'
-
   get 'signup', to: 'poets#new'
+
+  resources :poets
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
