2020f202e3bf9065a462cdf1a16f632ad914449d
Simon Dell <simon@expressionworks.co.uk>
2016-07-21 07:01:48 +0100

    7.3.4 Don't sign up invalid poets.

--- test/integration/poets_sign_up_test.rb
+++ test/integration/poets_sign_up_test.rb
@@ -1,12 +1,22 @@
 require 'test_helper'
 
 class PoetsSignUpTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
 
   test 'Can see sign-up page' do
     get signup_path
     assert_select 'title', full_title('Sign up')
+  end
+
+  test 'do not register invalid poet sign-ups' do
+    assert_no_difference 'Poet.count' do
+      post poets_path, { params: {
+        poet: {
+          name: '',
+          email: 'poet@nowhere',
+          password: 'nope',
+          password_confirmation: 'really',
+        }
+      }}
+    end
   end
 end
