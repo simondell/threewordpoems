12bac7dc6d228696c384d2e35858cf45b9a71a44
Simon Dell <simon@expressionworks.co.uk>
2016-07-26 21:47:04 +0100

    7.3.3 ex 3, 4 sign-up test now uses signup_path
    This seems pedantic. I know I've been up for getting precise about URLs
    in the past, but this seems a bit like doing it for the sake of it. I'm
    not convinced I'll remember all the parts.

--- test/integration/poets_sign_up_test.rb
+++ test/integration/poets_sign_up_test.rb
@@ -9,7 +9,7 @@ class PoetsSignUpTest < ActionDispatch::IntegrationTest
 
   test 'do not register invalid poet sign-ups' do
     assert_no_difference 'Poet.count' do
-      post poets_path, { params: {
+      post signup_path, { params: {
         poet: {
           name: '',
           email: 'poet@nowhere',
@@ -19,6 +19,7 @@ class PoetsSignUpTest < ActionDispatch::IntegrationTest
       }}
     end
 
+    assert_select 'form[action="/signup"]'
     assert_select '.field_with_errors'
     assert_select 'form section > h3'
   end
