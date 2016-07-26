a3331e038f05342bbbee8686afddd864e31d1017
Simon Dell <simon@expressionworks.co.uk>
2016-07-26 21:39:05 +0100

    7.3.3 ex 2 Updated sign-up path/routes
    using helpers in path names, adding routes etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
 
   root 'static_pages#home'
+
+  # static pages
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/styleguide', to: 'static_pages#styleguide'
-  get 'signup', to: 'poets#new'
 
+  # poets resource
   resources :poets
+  get 'signup', to: 'poets#new'
+  post 'signup', to: 'poets#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
