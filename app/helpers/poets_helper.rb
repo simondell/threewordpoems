96d979b77775ba3dc921a561d7454aafa5efef01
Simon Dell <simon@expressionworks.co.uk>
2016-07-26 21:53:59 +0100

    Fixing a bug

--- app/helpers/poets_helper.rb
+++ app/helpers/poets_helper.rb
@@ -3,7 +3,7 @@ module PoetsHelper
   def gravatar_for poet, options = { size: 50 }
     id = Digest::MD5::hexdigest poet.email.downcase
     size = options[:size]
-    url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    url = "https://secure.gravatar.com/avatar/#{id}?s=#{size}"
     image_tag url, alt: ''
   end
 end
