554f1bb6c870047d0c87393b6aefc9d9a0d6e84b
Simon Dell <simon@expressionworks.co.uk>
2016-07-26 21:54:16 +0100

    7.4.1 redirecting to the profile page after a signup

--- app/controllers/poets_controller.rb
+++ app/controllers/poets_controller.rb
@@ -2,7 +2,7 @@ class PoetsController < ApplicationController
   def create
     @poet = Poet.new user_params
     if @poet.save
-      # handle successful save
+      redirect_to @poet
     else
       render 'new'
     end
