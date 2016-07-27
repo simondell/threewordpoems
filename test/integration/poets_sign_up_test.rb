dcb41dc45e119372fb844cec68a5187e5e919126
Simon Dell <simon@expressionworks.co.uk>
2016-07-27 01:05:36 +0100

    7.4.2 ex 1..4 too tired to care

--- test/integration/poets_sign_up_test.rb
+++ test/integration/poets_sign_up_test.rb
@@ -23,4 +23,20 @@ class PoetsSignUpTest < ActionDispatch::IntegrationTest
     assert_select '.field_with_errors'
     assert_select 'form section > h3'
   end
+
+  test 'register valid sign-ups' do
+    assert_difference 'Poet.count', 1 do
+      post signup_path, { params: {
+        poet: {
+          name: 'Spirit in the Sky',
+          email: 'the_best@pla.ce',
+          password: 'spritualsky',
+          password_confirmation: 'spritualsky'
+        }
+      }}
+    end
+    follow_redirect!
+    assert_template 'poets/show'
+    assert_not flash.empty?
+  end
 end
