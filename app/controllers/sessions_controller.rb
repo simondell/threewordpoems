66c37b5ebd636006b0634501d1a9e3a327d9f6ac
Simon Dell <simon@expressionworks.co.uk>
2016-07-29 22:18:32 +0100

    8.1.5 failed login flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
+  def create
+    poet = Poet.find_by( email: params[:session][:email].downcase )
+    if poet && poet.authenticate(params[:session][:password])
+      # log in and redirect to Poet profile
+    else
+      flash.now[:error] = 'Do it again!!'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
   def new
   end
 end
