2d2a9508eea366dab3bc9666f5f858194a97c975
Simon Dell <simon@expressionworks.co.uk>
2016-08-09 00:09:13 +0100

    12.3.3 Password Reset Test

---
+++ test/integration/password_resets_test.rb
@@ -0,0 +1,77 @@
+require 'test_helper'
+
+class PasswordResetsTest < ActionDispatch::IntegrationTest
+  def setup
+    ActionMailer::Base.deliveries.clear
+    @poet = poets :test_poet
+  end
+
+  test 'password resets' do
+    get new_password_reset_path
+    assert_template 'password_resets/new'
+    # invalid email
+    post password_resets_path, params: {
+      password_reset: {
+        email: ''
+      }
+    }
+    assert_not flash.empty?
+    assert_template 'password_resets/new'
+    # valid email
+    post password_resets_path, params: {
+      password_reset: {
+        email: @poet.email
+      }
+    }
+    assert_not_equal @poet.reset_digest, @poet.reload.reset_digest
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    assert_not flash.empty?
+    assert_redirected_to root_url
+    # password reset form
+    poet = assigns :poet
+    # wrong email
+    get edit_password_reset_path poet.reset_token, { email: '' }
+    assert_redirected_to root_url
+    # inactive poet
+    poet.toggle! :activated
+    get edit_password_reset_path poet.reset_token, { email: poet.email }
+    assert_redirected_to root_url
+    poet.toggle! :activated
+    # right email, wrong token
+    get edit_password_reset_path 'wrong token', { email: poet.email }
+    assert_redirected_to root_url
+    # right email, right token
+    get edit_password_reset_path poet.reset_token, { email: poet.email }
+    assert_template 'password_resets/edit'
+    assert_select 'input[name=email][type=hidden][value=?]', poet.email
+    # invalid password & confirmation
+    patch password_reset_path(poet.reset_token), params: {
+      email: poet.email,
+      poet: {
+        password: 'foobz',
+        password_confirmation: 'sledboke'
+      }
+    }
+    assert flash.empty?
+    # empty password
+    patch password_reset_path(poet.reset_token), params: {
+      email: poet.email,
+      poet: {
+        password: '',
+        password_confirmation: ''
+      }
+    }
+    assert flash.empty?
+    # valid password & confirmation
+    patch password_reset_path(poet.reset_token), params: {
+      email: poet.email,
+      poet: {
+        password: 'foobaz',
+        password_confirmation: 'foobaz'
+      }
+    }
+    assert is_logged_in?
+    assert_not flash.empty?
+    assert_redirected_to poet
+  end
+end
