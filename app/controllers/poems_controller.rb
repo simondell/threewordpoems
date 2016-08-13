a43bd2640340becd42ea376b8c44b7c8f79eca56
Simon Dell <simon@expressionworks.co.uk>
2016-08-13 12:11:50 +0100

    13.3.2 Creating a poem - pt 2
    In which I learned
    - to render a collection with partial with a template
    - that you can specify where a partial and template reside using
      `'location/partial` so long as partials and their layout both begin
    with `'_'`s.
    - this means a data type can have a consistent markup but reside within
      different containers (e.g. `main > article` and `li > article` both
    refer to poems, one the focus of attention, the other in a list. To be
    fair, this'll get refactored out, probably, because the main view needs
    much more metadata and probablky different mark-up for an associated
    image.

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,13 +1,22 @@
 class PoemsController < ApplicationController
+  before_action :set_poem, only: [:destroy, :show]
   before_action :logged_in_poet, only: [:destroy]
-
-  def new
-    @poem = Poem.new
-  end
 
   def create
   end
 
   def destroy
+  end
+
+  def new
+    @poem = Poem.new
+  end
+
+  def show
+  end
+
+private
+  def set_poem
+    @poem = Poem.find params[:id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
 
-
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
   root 'static_pages#home'
 
   # static pages
@@ -19,7 +14,7 @@ Rails.application.routes.draw do
 
 
   # poems resource
-  resources :poems, only: [:create, :destroy, :new]
+  resources :poems, only: [:create, :destroy, :new, :show]
 
   # sessions "resource"
   get '/login', to: 'sessions#new'
@@ -31,6 +26,8 @@ Rails.application.routes.draw do
 
   # password resets resource
   resources :password_resets, only: [:create, :edit, :new, :update]
+  get 'password_resets/new'
+  get 'password_resets/edit'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
