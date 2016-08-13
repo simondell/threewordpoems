cfd76e111573a03e1c435b82ed69f9ff0c3fba05
Simon Dell <simon@expressionworks.co.uk>
2016-08-13 22:57:27 +0100

    13.3.2 Creating poems - fixed a bug in the tests
    Tests were failing intermittently. So frustrating! The fail was that
    certain poems weren't found on the poet profile page. At first I thought
    this was an ordering problem (and discovered that default_scope is
    largely frowned upon). Checking the test output more closely I realised
    it was an escaping issue. Model strings get escaped when written itno
    the HTML so & and ' become &amp; and &#39; respectively. The **tests**
    don't escape the model data when passing it to assert_match, so some of
    the poems wouldn't match despite appearing on the page in human terms.
    The fix was to use ERB::Util.h to escape the model property before
    handing it to assert_match.

--- test/integration/poets_profile_test.rb
+++ test/integration/poets_profile_test.rb
@@ -8,13 +8,13 @@ class PoetsProfileTest < ActionDispatch::IntegrationTest
   end
 
   test 'profile display' do
-    get poet_path @poet
+    get poet_path(@poet, page: 1)
     assert_template 'poets/show'
     assert_select 'title', full_title( @poet.name )
     assert_select 'img[src*="gravatar"]'
     assert_match @poet.poems.count.to_s, response.body
     @poet.poems.paginate(page: 1).each do |poem|
-      assert_match poem.content, response.body
+      assert_match ERB::Util.h(poem.content), response.body
     end
   end
 end
