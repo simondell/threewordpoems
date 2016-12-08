7e124ce26d18a87368df01bc1b9f1185e173e7fc
Simon Dell <simon@expressionworks.co.uk>
2016-12-08 16:32:00 +0000

    Attempting to list unpublished poems. It's not working yet.

--- app/controllers/admin/poems_controller.rb
+++ app/controllers/admin/poems_controller.rb
@@ -2,8 +2,10 @@ class Admin::PoemsController < ApplicationController
   before_action :has_editor_rights, only: [:index, :update]
 
   def index
-    @unpublished = Poem.unpublished.all
-    @published = Poem.published.all
+    # @unpublished = Poem.unpublished.paginate page: params[:page]
+    @unpublished = Poem.unpublished
+byebug
+    # @published = Poem.published.all
   end
 
   def update
